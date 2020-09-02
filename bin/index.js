2e7e569d36a7d285f82debcd9986174c3d705239
yangss3 <yss_2016@outlook.com>
2020-09-02 16:29:32 +0800

    update

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 const { program } = require('commander')
-const os = require('os')
-const path = require('path')
-const fs = require('fs-extra')
 const enhanceErrorMessages = require('../lib/utils/enhanceErrorMessages')
+const { execCmd } = require('../lib/utils/helper')
 
 program
   .version(
     `git-webhooks-server v${require('../package.json').version}`,
-    '-v,--version'
+    '-v, --version'
   )
   .name('git-webhooks-server')
   .usage('<command> [options]')
@@ -17,27 +15,43 @@ program
   .command('start')
   .description('Start an webhooks server for auto deployment')
   .action(() => {
-
       require('../lib/start.js')()
-
   })
 
 program
   .command('restart')
   .description('Restart the webhooks server')
-  .action(() => {
-    // require('../lib/restart.js')()
+  .action(async () => {
+    try {
+      const {stdout, stderr} = await execCmd('pm2 restart webhooks')
+      if(stdout) {
+        console.log('Restart. Done')
+      }
+      console.error(stderr)
+    } catch (error) {
+      console.error(error)
+    }
   })
 
 program
   .command('stop')
   .description('Stop the webhooks server')
-  .action(() => {})
+  .action(async () => {
+    try {
+      const {stdout, stderr} = await execCmd('pm2 stop webhooks')
+      if(stdout) {
+        console.log('Stop. Done.')
+      }
+      console.error(stderr)
+    } catch (error) {
+      console.error(error)
+    }
+  })
 
 program
-  .command('config <project-name>')
+  .command('config [project-name]')
   .option('-l, --list', 'List configurations')
-  .description('Create a project template')
+  .description(`Create or list a project's configuration`)
   .action((projectName, cmd) => {
     require('../lib/config.js')(projectName, cmd.list)
   })
